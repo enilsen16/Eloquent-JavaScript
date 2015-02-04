26a9f77222bbe9a280ec4d4caacdbcff8ee428b3
Erik Nilsen <enilsen16@live.com>
2015-02-04 12:41:10 -0800

    add chapter 2 and first test

---
+++ ch2/looping.js
@@ -0,0 +1,5 @@
+var app = exports = module.exports = {};
+
+app.triangle = function() {
+  return '#\n'
+};
---
+++ ch2/looping_test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var actual = require ('../ch2/looping');
+
+describe ('looping', function() {
+  it('return 1 #', function(){
+    expect(actual.triangle(1)).to.eql('#\n');
+  });
+});
